c922db8a808eabae8f0d1d3ce7bfd55ec0a45d13
A OLABY <abdulmohaemn@Abdulmohaemns-MacBook-Pro.local>
2023-12-21 10:22:21 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+//////////////
+/////JQUERY//////
+/////////////
+
+
+jQuery(document).ready(function(){
+    "user trick"
+    $('.slider').ripples({
+        dropRadius: 10,
+        perturbance: 0.01,
+    });
+});
